8be955748de2b46fdee2b839c5ccbf6413483b4a
Vlad <vladi003@yandex.ru>
2022-04-18 11:23:17 +0300

    fixes

--- football_school/settings.py
+++ football_school/settings.py
@@ -107,13 +107,13 @@ WSGI_APPLICATION = 'football_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'mydatabase',
     }
-}
-"""DATABASES = {
+}"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': DB_NAME,
@@ -125,7 +125,7 @@ DATABASES = {
         },
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
